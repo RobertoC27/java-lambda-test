3da7492f6a8dec293e3bda53a9f7bfb177d85fdd
RobertoC27 <chiroy_134@hotmail.com>
2020-03-09 12:12:02 -0600

    create handler

--- src/main/java/gt/com/celera/App.java
+++ src/main/java/gt/com/celera/App.java
@@ -1,13 +1,18 @@
 package gt.com.celera;
 
+import java.security.SecureRandom;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App implements RequestHandler<RequestClass, ResponseClass> {
+    public ResponseClass handleRequest(RequestClass request, Context context) {
+        SecureRandom random = new SecureRandom();
+        String responseString = String.format("Hey there %s, rand is %d", request.getFirstName(), random.nextInt());
+        return new ResponseClass(responseString);
     }
 }
